c5f9c2105734fc5d77099e259e1266fa0ec74109
Abigail Trapp <abbsthepandahead@gmail.com>
2022-02-11 21:17:11 -0700

    JavaScript pt 2 setupt

--- code.js
+++ code.js
@@ -4,12 +4,23 @@ function divisibleSumPairs(n, k, ar) {
     // return two number pairs
     var result = 0
       for(let i = 0; i < n; i++){
+        // console.log(ar[i])
         for(let j = 1; j < n - i ; j++){
+          // console.log(ar[j])
           if((ar[i] + ar[j]) % k === 0){
             result ++
           } 
         } 
       } 
       return result
+
     }
-    console.log(divisibleSumPairs(2, 5, [2,1]))
+    console.log(divisibleSumPairs(6, 3, [1,3,4,6,9,10]))
+    let body = document.querySelector("body")
+    let button = document.getElementById("button")
+    button.addEventListener("click", function(){
+    let input = document.getElementById("input").value
+    let div = document.createElement("div")
+    div.innerHTML = `${divisibleSumPairs(input)}`
+    body.append(div)
+  })
